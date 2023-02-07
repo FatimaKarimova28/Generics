317e458b177ad203e41948e7d41beca86708c80a
FatimaKarimova28 <fatimaika@code.edu.az>
2023-02-07 16:02:08 +0400

    Done

---
+++ Generics/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace Generics
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            FindType("Salam");
+
+
+            DoSwapValue<int>(10, 15);
+        }
+
+
+
+
+        public static void FindType<T>(T type)
+        {
+            Console.WriteLine(type);
+        }
+
+
+
+
+        public static void DoSwapValue<T>(T v1, T v2)
+        {
+            T v = default;
+            T temporary = v;
+            v = v1;
+            v1 = v2;
+            v2 = v;
+            Console.WriteLine($"{v1} {v2}");
+        }
+    }
+}
